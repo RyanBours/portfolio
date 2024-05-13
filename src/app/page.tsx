0f5312e10f0c3ea4d518a6521944da5a0fc2d5e6
Ryan Bours <ryan.bours@gmail.com>
2024-05-13 06:16:15 +0200

    added cv download btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import ProjectCard, { ProjectRepoIcon } from '@/components/ProjectCard';
 import { Footer } from '@/components/Footer';
 import { IconAzure, IconBootstrap, IconBun, IconCsharp, IconGPT, IconGoogleCloud, IconGraphQL, IconJavascript, IconJquery, IconKubernetes, IconLangchain, IconLaravel, IconLua, IconMQTT, IconMathplotlib, IconMySQL, IconNestjs, IconNextjs, IconOnnx, IconPHP, IconPandas, IconPinecone, IconPostgres, IconPrisma, IconPython, IconReact, IconRust, IconSass, IconSciKitLearn, IconSvelte, IconTailwind, IconTauri, IconTensorflow, IconTerraform, IconThree, IconTypescript, IconVercel } from '@/components/Icons';
 import { Skill, SkillProficiency } from '@/components/Skill';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -20,13 +21,11 @@ export default function Home() {
 // MARK: About Section
 const AboutSection = () => {
   return (
-    <section id='about'>
-      <h2>About</h2>
-      <p>
-        I&apos;m a software engineer and a freelance developer. I enjoy working
-        on projects that challenge me to learn new things and solve interesting
-        problems.
-      </p>
+    <section id='about' className=''>
+
+      {/* <div className='size-16'>
+        <Image src="/profile.jpg" fill={true} alt="A picture of me(Ryan Bours)" />
+      </div> */}
 
       {/* rough notes: */}
       {/* 
@@ -161,6 +160,12 @@ const ContactSection = () => {
           <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
         </svg>
         <span className='ml-2'>ryan.bours@gmail.com</span>
+      </a>
+      <a href="/cv_ryan_bours_softwaredeveloper_v2.pdf" download className='mt-2 pl-4 flex text-gray-950  dark:text-white'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+        </svg>
+        <span className='ml-2'>Download CV</span>
       </a>
     </section>
   );
